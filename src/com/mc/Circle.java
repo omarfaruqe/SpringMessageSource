198ff0b21f1c0a883a625b337abc77972d65dacd
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-11 22:49:58 -0400

    Spring message source from property file

---
+++ src/com/mc/Circle.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mc;
+
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+/**
+ *
+ * @author faruqe
+ */
+public class Circle implements Shape{
+    private Point center;
+    
+    @Autowired
+    private MessageSource messageSource;
+
+    public MessageSource getMessageSource() {
+        return messageSource;
+    }
+
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+    
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    @Override
+    public void draw() {        
+        System.out.println("Circle Drawn: "+getCenter().getX()+", "+getCenter().getY());
+        System.out.println(this.messageSource.getMessage("greeting", null, "Default greeting", Locale.CANADA));
+        System.out.println(this.messageSource.getMessage("drawing.circle", null, "Default greeting", Locale.CANADA));
+        System.out.println(this.messageSource.getMessage("drawing.point", new Object[]{center.getX(), center.getY()}, "Default point greeting", Locale.CANADA));
+    }
+    
+}
---
+++ src/com/mc/SpringMessageSource.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mc;
+
+import java.util.Locale;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author faruqe
+ */
+public class SpringMessageSource {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        Shape shape=(Shape)context.getBean("circleId");
+        shape.draw();
+        //System.out.println(context.getMessage("greeting", null, "Default greeting", Locale.CANADA));
+    }
+    
+}
